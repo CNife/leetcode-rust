d91947e6ea21cdc6b94df824c96e32bce6361048
CNife <CNife@vip.qq.com>
2021-03-31 21:58:30 +0800

    添加helper.ps1脚本 更新editorconfig

---
+++ helper.ps1
@@ -0,0 +1,35 @@
+$ErrorActionPreference = 'Stop'
+
+function Invoke-CargoTest {
+    Set-Location $PSScriptRoot
+    cargo test --package finished
+}
+
+$LibRS = "$PSScriptRoot\src\lib.rs"
+
+if (!((Get-Content -First 1 $LibRS) -match '(?:pub\s+)?mod\s+(\S+)\s*;'))
+{
+    Write-Host "没有找到可移动的模块"
+    Invoke-CargoTest
+    exit
+}
+$ModuleName = $Matches[1]
+
+$LibRSContent = Get-Content $LibRS
+if ($LibRSContent.Count -eq 1)
+{
+    Clear-Content $LibRS
+}
+else
+{
+    $LibRSContent[1..($LibRSContent.Count - 1)] | Set-Content -Path $LibRS
+}
+
+Move-Item -Path "$PSScriptRoot\src\$ModuleName.rs" -Destination "$PSScriptRoot\finished\src\$ModuleName.rs"
+
+$FinishedLibRS = "$PSScriptRoot\finished\src\lib.rs"
+$Modules = Get-Content $FinishedLibRS
+$Modules += "pub mod $ModuleName;"
+$Modules | Sort-Object | Set-Content $FinishedLibRS
+
+Invoke-CargoTest
